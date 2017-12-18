a8a922ee286abedba584d258fba1e3e5ef9ef15f
Mário Garrido <mrfhitz@users.noreply.github.com>
2017-12-18 21:49:20 +0000

    fix metadata webpage title

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<title><?php echo page_title('Page can’t be found'); ?> - <?php echo site_name(); ?></title>
+		<title><?php echo site_name(); ?> - <?php echo page_title('Page can’t be found'); ?></title>
 
 		<meta name="description" content="<?php echo site_description(); ?>">
 
